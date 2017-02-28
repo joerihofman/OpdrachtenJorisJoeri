d00e592e0bf0dcc0f8ba67c192387e75cbe75fbc
jorisjjansen <jorisjip@gmail.com>
2017-02-28 12:16:01 +0100

    opdracht 30_4 ==> threads

--- Week1Uitwerkingen/src/opdracht30_4.java
+++ Week1Uitwerkingen/src/opdracht30_4.java
@@ -1,5 +1,12 @@
-/**
- * Created by MSI on 28-2-2017.
- */
-public class opdracht30_4 {
+public class opdracht30_4 implements Runnable {
+    public void run() {
+        System.out.println("This is a test.");
+    }
+
+    public static void main(String args[]) {
+        for (int i = 0; i < 1000; i++) {
+            (new Thread(new opdracht30_4())).start();
+        }
+
+    }
 }
