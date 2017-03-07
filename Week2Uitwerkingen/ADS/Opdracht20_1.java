6e8a0ca711e65f247181ca0fb77ba538b8d8388a
jorisjjansen <jorisjip@gmail.com>
2017-03-07 12:58:31 +0100

    Final 20_1

--- Week2Uitwerkingen/ADS/Opdracht20_1.java
+++ Week2Uitwerkingen/ADS/Opdracht20_1.java
@@ -1,28 +1,29 @@
 import java.io.IOException;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
-import java.util.Scanner;
+import java.util.*;
 
 public class Opdracht20_1{
     public static void main(String[] args) throws IOException {
         List<String> originalList = textToList("Week2Uitwerkingen/ADS/textfile.txt");
+        Collections.sort(originalList);
         Collections.reverse(originalList);
-
-        for(int i=0; i< originalList.size(); i++){
-            System.out.println(originalList.get(i));
-        }
     }
 
     public static List textToList(String pathToFile) throws IOException {
+        //inspiratie: http://stackoverflow.com/questions/3806062/how-to-open-a-txt-file-and-read-numbers-in-java
         Path filePath = Paths.get(pathToFile);
         Scanner scanner = new Scanner(filePath);
         List<String> words = new ArrayList<>();
 
         while (scanner.hasNext()) {
-            words.add(scanner.next());
+            String word = scanner.next();
+            if (word.matches(".*\\d+.*")) {
+                ;
+            }
+            else {
+                words.add(word);
+            }
         }
 
         return words;
