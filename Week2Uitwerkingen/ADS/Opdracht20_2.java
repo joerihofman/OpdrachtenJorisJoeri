0ad1c9b05ae2d2e53db8648d3310f5076b1cb421
qwerty12794 <onzin09@gmail.com>
2017-03-07 13:07:47 +0100

    opdracht 20_2 mee bezig, gui is bijna af; rest niet

--- Week2Uitwerkingen/ADS/Opdracht20_2.java
+++ Week2Uitwerkingen/ADS/Opdracht20_2.java
@@ -1,5 +1,74 @@
+import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.text.Text;
+import javafx.stage.Stage;
+
 /**
  * Created by MSI on 7-3-2017.
  */
-public class Opdracht20_2 {
+public class Opdracht20_2 extends Application{
+    public static void main(String[] args) {launch(args);}
+
+    @Override
+    public void start(Stage primaryStage) throws Exception{
+        primaryStage.setTitle("GEEF ANTWOORD~~~!");
+        GridPane gridPane = new GridPane();
+        gridPane.setAlignment(Pos.CENTER);
+        gridPane.setVgap(10);
+        gridPane.setHgap(10);
+        gridPane.setPadding(new Insets(25,25,25,25));
+
+        Text text = new Text("Wat een mooi programma");
+        gridPane.add(text,0,0);
+
+        Label invoer = new Label("Voer je invoer in: ");
+        gridPane.add(invoer,0,2);
+
+        TextField textField = new TextField();
+        gridPane.add(textField,1,2);
+
+        Button button_sort = new Button("SORT");
+        Button button_shuffle = new Button("SHUFFLE");
+        Button button_reverse = new Button("REVERSE");
+        HBox hBox = new HBox(10);
+        hBox.setAlignment(Pos.BOTTOM_RIGHT);
+        hBox.getChildren().add(button_sort);
+        hBox.getChildren().add(button_shuffle);
+        hBox.getChildren().add(button_reverse);
+        gridPane.add(hBox,1,3);
+
+        button_sort.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent event) {
+                ;
+            }
+        });
+
+        button_shuffle.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent event) {
+                ;
+            }
+        });
+
+        button_reverse.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent event) {
+                ;
+            }
+        });
+
+        Scene scene = new Scene(gridPane,550,300);
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
 }
