aa2a6739435c2e72d47de650145aad8cff3a4747
jorisjjansen <jorisjip@gmail.com>
2017-03-07 11:46:52 +0100

    20_1 bijna klaar

--- Week2Uitwerkingen/src/ADS/Opdracht20_1.java
+++ Week2Uitwerkingen/src/ADS/Opdracht20_1.java
@@ -1,7 +1,34 @@
 package ADS;
 
-public class Opdracht20_1 {
-    public static void main(String[] args){
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
 
+public class Opdracht20_1{
+    public static void main(String[] args){
+        List<String> originalList = textToList("textfile.txt");
+        List<String> sortedList = new ArrayList<>();
+
+        for(int item=0; item < sortedList.size(); item++){
+            System.out.println(item)
+        }
+    }
+
+    public ArrayList textToList(pathToFile){
+        Path filePath = Paths.get(pathToFile);
+        Scanner scanner = new Scanner(filePath);
+        List<String> words = new ArrayList<>();
+
+        while (scanner.hasNext()) {
+            if (scanner.hasNextInt()) {
+                words.add(scanner.next());
+            } else {
+                scanner.next();
+            }
+        }
+
+        return words;
     }
 }
