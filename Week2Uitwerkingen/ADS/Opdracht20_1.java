886e8f084f6dbbdb252f19db7c6abc9180d329cc
qwerty12794 <onzin09@gmail.com>
2017-03-07 12:05:14 +0100

    bestand aangepast

--- Week2Uitwerkingen/ADS/Opdracht20_1.java
+++ Week2Uitwerkingen/ADS/Opdracht20_1.java
@@ -8,6 +8,7 @@ import java.util.Scanner;
 public class Opdracht20_1{
     public static void main(String[] args) throws IOException {
         List<String> originalList = textToList("Week2Uitwerkingen/ADS/textfile.txt");
+        System.out.println(originalList);
         List<String> sortedList = new ArrayList<>();
 
         for(int item=0; item < sortedList.size(); item++){
@@ -21,11 +22,7 @@ public class Opdracht20_1{
         List<String> words = new ArrayList<>();
 
         while (scanner.hasNext()) {
-            if (scanner.hasNextInt()) {
-                words.add(scanner.next());
-            } else {
-                scanner.next();
-            }
+            words.add(scanner.next());
         }
 
         return words;
